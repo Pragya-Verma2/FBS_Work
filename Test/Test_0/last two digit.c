995a9ebd27107f162b89b342a8a82bbb55d139d4
Pragya-Verma2 <pragyagurjar23@gmail.com>
2025-07-29 21:57:50 +0530

    last two digit

---
+++ Test/Test_0/last two digit.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+int main(){
+//	int number = 223410;
+//	int lastTwo;
+//	
+//	lastTwo = number % 100;
+//	printf("Last Two Digit : %d",lastTwo);
+
+
+int number = 223410;
+	int lastTwo;
+	
+	number = number / 10;
+	
+
+	printf("Last Two Digit : %d",lastTwo);
+}
