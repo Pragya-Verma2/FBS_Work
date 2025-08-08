9aa7571cbb9cf9a504cfe8ac366b018416e39b05
Pragya-Verma2 <pragyagurjar23@gmail.com>
2025-08-08 21:07:01 +0530

    even odd number of range

---
+++ Test/Test_2/even odd range.c
@@ -0,0 +1,18 @@
+# include<stdio.h>
+int main(){
+
+for(int start=10;start<=25;start++){	
+	if(start % 2!=1){
+		printf("%d ", start);
+	}
+}
+printf("\n");
+
+for(int start=1;start<=15;start++){	
+	if(start % 2==0){
+		printf("%d ", start);
+	}
+}
+
+
+}
