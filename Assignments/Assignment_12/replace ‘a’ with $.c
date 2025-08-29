f17c5d950739ed03264d6d970f23c8cc0b62171b
Pragya-Verma2 <pragyagurjar23@gmail.com>
2025-08-29 22:43:25 +0530

    Add files via upload

---
+++ Assignments/Assignment_12/replace ‘a’ with $.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+#include <string.h>
+int main(){
+	char str[50];
+
+	printf("enter a string:");
+	scanf("%s",str);
+	
+	int size=strlen(str);
+	
+	for(int i=0;i<size;i++){
+		if(str[i]=='a'){
+			str[i]='$';
+		}
+	}
+   printf("%s",str);
+}
